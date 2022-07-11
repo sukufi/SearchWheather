6d53c2e5720f395e5ce0d9da292c829ecd9ca43a
sukufi <60154684+sukufi@users.noreply.github.com>
2022-07-11 16:30:14 +0300

    added

--- learn_weather.py
+++ learn_weather.py
@@ -1,11 +1,15 @@
 import requests
-import json
 
 API_key = "1b3a4a2010cfd8eb135628c812b425e3"
 city = input("City: ")
-#response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q=London&limit=&appid={API_key}")
 
 response= requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}")
 
 obj = response.json()
-print(obj)
+
+temp = round(obj["main"]["temp"] - 273.15)
+temp_max = round(obj["main"]["temp_max"] - 273.15)
+temp_min = round(obj["main"]["temp_min"] - 273.15)
+country = obj["sys"]["country"]
+print(f"This is the weather in {obj['name']}, {country}")
+print(f"Now: {temp}°C , Todays max: {temp_max}°C, Todays low: {temp_min}°C ")
